620dfb3b956670be18f3ab084eb7a223f4f9cca5
DevExpressExampleBot <fakeEmail>
2022-01-26 20:24:41 +0300

    Source auto update [skip ci]

--- VB/WorkbookProgressSample/Form1.Designer.vb
+++ VB/WorkbookProgressSample/Form1.Designer.vb
@@ -1,117 +1,117 @@
-﻿Namespace WorkbookProgressSample
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
+Namespace WorkbookProgressSample
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-'INSTANT VB NOTE: The variable resources was renamed since it may cause conflicts with calls to static members of the user-defined type with this name:
-			Dim resources_Conflict As New System.ComponentModel.ComponentResourceManager(GetType(Form1))
-			Me.label1 = New System.Windows.Forms.Label()
-			Me.progressBarLoad = New System.Windows.Forms.ProgressBar()
-			Me.label2 = New System.Windows.Forms.Label()
-			Me.progressBarExport = New System.Windows.Forms.ProgressBar()
-			Me.btnRunCancel = New System.Windows.Forms.Button()
-			Me.SuspendLayout()
-			' 
-			' label1
-			' 
-			Me.label1.AutoSize = True
-			Me.label1.Location = New System.Drawing.Point(20, 16)
-			Me.label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
-			Me.label1.Name = "label1"
-			Me.label1.Size = New System.Drawing.Size(113, 13)
-			Me.label1.TabIndex = 0
-			Me.label1.Text = "Loading a workbook..."
-			' 
-			' progressBarLoad
-			' 
-			Me.progressBarLoad.Anchor = (CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles))
-			Me.progressBarLoad.Location = New System.Drawing.Point(22, 41)
-			Me.progressBarLoad.Margin = New System.Windows.Forms.Padding(2)
-			Me.progressBarLoad.Name = "progressBarLoad"
-			Me.progressBarLoad.Size = New System.Drawing.Size(380, 19)
-			Me.progressBarLoad.TabIndex = 1
-			' 
-			' label2
-			' 
-			Me.label2.AutoSize = True
-			Me.label2.Location = New System.Drawing.Point(20, 84)
-			Me.label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
-			Me.label2.Name = "label2"
-			Me.label2.Size = New System.Drawing.Size(96, 13)
-			Me.label2.TabIndex = 2
-			Me.label2.Text = "Exporting to PDF..."
-			' 
-			' progressBarExport
-			' 
-			Me.progressBarExport.Anchor = (CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles))
-			Me.progressBarExport.Location = New System.Drawing.Point(22, 109)
-			Me.progressBarExport.Margin = New System.Windows.Forms.Padding(2)
-			Me.progressBarExport.Name = "progressBarExport"
-			Me.progressBarExport.Size = New System.Drawing.Size(380, 19)
-			Me.progressBarExport.TabIndex = 3
-			' 
-			' btnRunCancel
-			' 
-			Me.btnRunCancel.Anchor = (CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles))
-			Me.btnRunCancel.Location = New System.Drawing.Point(314, 162)
-			Me.btnRunCancel.Margin = New System.Windows.Forms.Padding(2)
-			Me.btnRunCancel.Name = "btnRunCancel"
-			Me.btnRunCancel.Size = New System.Drawing.Size(89, 28)
-			Me.btnRunCancel.TabIndex = 4
-			Me.btnRunCancel.Text = "Run"
-			Me.btnRunCancel.UseVisualStyleBackColor = True
-'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
-'ORIGINAL LINE: this.btnRunCancel.Click += new System.EventHandler(this.RunCancel_Click);
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(430, 222)
-			Me.Controls.Add(Me.btnRunCancel)
-			Me.Controls.Add(Me.progressBarExport)
-			Me.Controls.Add(Me.label2)
-			Me.Controls.Add(Me.progressBarLoad)
-			Me.Controls.Add(Me.label1)
-			Me.Icon = (CType(resources_Conflict.GetObject("$this.Icon"), System.Drawing.Icon))
-			Me.Margin = New System.Windows.Forms.Padding(2)
-			Me.Name = "Form1"
-			Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-			Me.Text = "Progress Dialog"
-'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
-'ORIGINAL LINE: this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
-			Me.ResumeLayout(False)
-			Me.PerformLayout()
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(WorkbookProgressSample.Form1))
+            Me.label1 = New System.Windows.Forms.Label()
+            Me.progressBarLoad = New System.Windows.Forms.ProgressBar()
+            Me.label2 = New System.Windows.Forms.Label()
+            Me.progressBarExport = New System.Windows.Forms.ProgressBar()
+            Me.btnRunCancel = New System.Windows.Forms.Button()
+            Me.SuspendLayout()
+            ' 
+            ' label1
+            ' 
+            Me.label1.AutoSize = True
+            Me.label1.Location = New System.Drawing.Point(20, 16)
+            Me.label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+            Me.label1.Name = "label1"
+            Me.label1.Size = New System.Drawing.Size(113, 13)
+            Me.label1.TabIndex = 0
+            Me.label1.Text = "Loading a workbook..."
+            ' 
+            ' progressBarLoad
+            ' 
+            Me.progressBarLoad.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) Or System.Windows.Forms.AnchorStyles.Right)), System.Windows.Forms.AnchorStyles)
+            Me.progressBarLoad.Location = New System.Drawing.Point(22, 41)
+            Me.progressBarLoad.Margin = New System.Windows.Forms.Padding(2)
+            Me.progressBarLoad.Name = "progressBarLoad"
+            Me.progressBarLoad.Size = New System.Drawing.Size(380, 19)
+            Me.progressBarLoad.TabIndex = 1
+            ' 
+            ' label2
+            ' 
+            Me.label2.AutoSize = True
+            Me.label2.Location = New System.Drawing.Point(20, 84)
+            Me.label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+            Me.label2.Name = "label2"
+            Me.label2.Size = New System.Drawing.Size(96, 13)
+            Me.label2.TabIndex = 2
+            Me.label2.Text = "Exporting to PDF..."
+            ' 
+            ' progressBarExport
+            ' 
+            Me.progressBarExport.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) Or System.Windows.Forms.AnchorStyles.Right)), System.Windows.Forms.AnchorStyles)
+            Me.progressBarExport.Location = New System.Drawing.Point(22, 109)
+            Me.progressBarExport.Margin = New System.Windows.Forms.Padding(2)
+            Me.progressBarExport.Name = "progressBarExport"
+            Me.progressBarExport.Size = New System.Drawing.Size(380, 19)
+            Me.progressBarExport.TabIndex = 3
+            ' 
+            ' btnRunCancel
+            ' 
+            Me.btnRunCancel.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right)), System.Windows.Forms.AnchorStyles)
+            Me.btnRunCancel.Location = New System.Drawing.Point(314, 162)
+            Me.btnRunCancel.Margin = New System.Windows.Forms.Padding(2)
+            Me.btnRunCancel.Name = "btnRunCancel"
+            Me.btnRunCancel.Size = New System.Drawing.Size(89, 28)
+            Me.btnRunCancel.TabIndex = 4
+            Me.btnRunCancel.Text = "Run"
+            Me.btnRunCancel.UseVisualStyleBackColor = True
+            AddHandler Me.btnRunCancel.Click, New System.EventHandler(AddressOf Me.RunCancel_Click)
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(430, 222)
+            Me.Controls.Add(Me.btnRunCancel)
+            Me.Controls.Add(Me.progressBarExport)
+            Me.Controls.Add(Me.label2)
+            Me.Controls.Add(Me.progressBarLoad)
+            Me.Controls.Add(Me.label1)
+            Me.Icon = CType((resources.GetObject("$this.Icon")), System.Drawing.Icon)
+            Me.Margin = New System.Windows.Forms.Padding(2)
+            Me.Name = "Form1"
+            Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+            Me.Text = "Progress Dialog"
+            AddHandler Me.FormClosing, New System.Windows.Forms.FormClosingEventHandler(AddressOf Me.Form1_FormClosing)
+            Me.ResumeLayout(False)
+            Me.PerformLayout()
+        End Sub
 
-		Private label1 As System.Windows.Forms.Label
-		Private progressBarLoad As System.Windows.Forms.ProgressBar
-		Private label2 As System.Windows.Forms.Label
-		Private progressBarExport As System.Windows.Forms.ProgressBar
-		Private WithEvents btnRunCancel As System.Windows.Forms.Button
-	End Class
+#End Region
+        Private label1 As System.Windows.Forms.Label
+
+        Private progressBarLoad As System.Windows.Forms.ProgressBar
+
+        Private label2 As System.Windows.Forms.Label
+
+        Private progressBarExport As System.Windows.Forms.ProgressBar
+
+        Private btnRunCancel As System.Windows.Forms.Button
+    End Class
 End Namespace
-
--- VB/WorkbookProgressSample/Form1.vb
+++ VB/WorkbookProgressSample/Form1.vb
@@ -1,52 +1,51 @@
-﻿Imports System
+Imports System
 Imports System.Threading
 Imports System.Windows.Forms
 Imports DevExpress.Spreadsheet
 
 Namespace WorkbookProgressSample
-	Partial Public Class Form1
-		Inherits Form
 
-		Private cancellationSource As CancellationTokenSource
+    Public Partial Class Form1
+        Inherits Form
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+        Private cancellationSource As CancellationTokenSource
 
-		Private Async Sub RunCancel_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnRunCancel.Click
-			If Not Me.btnRunCancel.IsHandleCreated Then Return
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-			If cancellationSource IsNot Nothing Then
-				cancellationSource.Cancel()
-			Else
-				progressBarLoad.Value = 0
-				progressBarExport.Value = 0
-				btnRunCancel.Text = "Cancel"
-				cancellationSource = New CancellationTokenSource()
-				Try
-					Using workbook As New Workbook()
-						Await workbook.LoadDocumentAsync("Document.xlsx", cancellationSource.Token, New Progress(Of Integer)(Sub(progress)
-							progressBarLoad.Value = progress
-							progressBarLoad.Refresh()
-						End Sub))
-						Await workbook.ExportToPdfAsync("Result.pdf", cancellationSource.Token, New Progress(Of Integer)(Sub(progress)
-							progressBarExport.Value = progress
-							progressBarExport.Refresh()
-						End Sub))
-					End Using
-				Catch e1 As OperationCanceledException
-					progressBarLoad.Value = 0
-					progressBarExport.Value = 0
-				Finally
-					cancellationSource.Dispose()
-					cancellationSource = Nothing
-					btnRunCancel.Text = "Run"
-				End Try
-			End If
-		End Sub
+        Private Async Sub RunCancel_Click(ByVal sender As Object, ByVal e As EventArgs)
+            If cancellationSource IsNot Nothing Then
+                cancellationSource.Cancel()
+            Else
+                progressBarLoad.Value = 0
+                progressBarExport.Value = 0
+                btnRunCancel.Text = "Cancel"
+                cancellationSource = New CancellationTokenSource()
+                Try
+                    Using workbook As Workbook = New Workbook()
+                        Await workbook.LoadDocumentAsync("Document.xlsx", cancellationSource.Token, New Progress(Of Integer)(Sub(progress)
+                            progressBarLoad.Value = progress
+                            progressBarLoad.Refresh()
+                        End Sub))
+                        Await workbook.ExportToPdfAsync("Result.pdf", cancellationSource.Token, New Progress(Of Integer)(Sub(progress)
+                            progressBarExport.Value = progress
+                            progressBarExport.Refresh()
+                        End Sub))
+                    End Using
+                Catch __unusedOperationCanceledException1__ As OperationCanceledException
+                    progressBarLoad.Value = 0
+                    progressBarExport.Value = 0
+                Finally
+                    cancellationSource.Dispose()
+                    cancellationSource = Nothing
+                    btnRunCancel.Text = "Run"
+                End Try
+            End If
+        End Sub
 
-		Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As FormClosingEventArgs) Handles Me.FormClosing
-			cancellationSource?.Cancel()
-		End Sub
-	End Class
+        Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As FormClosingEventArgs)
+            cancellationSource?.Cancel()
+        End Sub
+    End Class
 End Namespace
